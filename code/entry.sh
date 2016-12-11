ce08e63c0bd984bc1ecb83061968efed72cf68b1
thomas <sketchbang@gmail.com>
2016-12-11 10:25:26 -0600

    remove code

--- code/entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# npm i
-# npm i -g elm@^0.18.0
-# elm-package install -y
-# chmod 766 -R elm-stuff
-exec "$@"
--- code/src/vendor.js
+++
@@ -1,4 +0,0 @@
-require('ace-css/css/ace.css');
-require('font-awesome/css/font-awesome.css');
-require('webcomponents.js');
-require('d3');
